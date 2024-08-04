42fbb2bcfdf2722a33145ce67ee35d3d2c734eba
akrati <akrati0305@gmail.com>
2024-08-04 16:23:31 +0530

    hmm

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,40 +1,41 @@
-export type IBlogDetail = {
-    created_at: string;
-    id: string;
-    image_url: string;
-    is_premium: boolean;
-    is_published: boolean;
-    title: string;
-    blog_content: {
-        blog_id: string;
-        content: string;
+export type ITag = {
+        id: number;
+        name: string;
+    };
+    
+    export type ICategory = {
+        id: number;
+        name: string;
+    };
+    
+    export type IBlogDetail = {
         created_at: string;
-        id:string;
-        image_url:string;
-        is_premium:boolean;
-        is_published:boolean;
-        title:string;
-        blog_content:{
-            blog_id:string;
-            content:string;
-            created_at:string;
+        id: string;
+        image_url: string;
+        is_premium: boolean;
+        is_published: boolean;
+        title: string;
+        blog_content: {
+            blog_id: string;
+            content: string;
+            created_at: string;
         } | null;
-
-} | null;
-
-
-export type IBlog = {
+        tags: ITag[];
+        categories: ICategory[];
+    } | null;
+    
+    export type IBlog = {
         created_at: string;
-        id:string;
-        image_url:string;
-        is_premium:boolean;
-        is_published:boolean;
-        title:string;
-       
-
-} | null;
-
-export type Iuser ={
+        id: string;
+        image_url: string;
+        is_premium: boolean;
+        is_published: boolean;
+        title: string;
+        tags: ITag[];
+        categories: ICategory[];
+    } | null;
+    
+    export type Iuser = {
         created_at: string;
         display_name: string;
         email: string;
@@ -45,3 +46,4 @@ export type Iuser ={
         stripe_subscription_id: string | null;
         subscription_status: boolean;
     } | null;
+    
