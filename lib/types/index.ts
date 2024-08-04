316d4286674d5c3f98bb5c7cfb45035d48456766
abhinavpatel0 <121202898+abhinavpatel0@users.noreply.github.com>
2024-08-04 09:46:37 +0530

    Update index.ts

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,38 +1,48 @@
+export type ITag = {
+    id: number;
+    name: string;
+};
+
+export type ICategory = {
+    id: number;
+    name: string;
+};
+
 export type IBlogDetail = {
+    created_at: string;
+    id: string;
+    image_url: string;
+    is_premium: boolean;
+    is_published: boolean;
+    title: string;
+    blog_content: {
+        blog_id: string;
+        content: string;
         created_at: string;
-        id:string;
-        image_url:string;
-        is_premium:boolean;
-        is_published:boolean;
-        title:string;
-        blog_content:{
-            blog_id:string;
-            content:string;
-            created_at:string;
-        } | null;
-
+    } | null;
+    tags: ITag[];
+    categories: ICategory[];
 } | null;
-
 
 export type IBlog = {
-        created_at: string;
-        id:string;
-        image_url:string;
-        is_premium:boolean;
-        is_published:boolean;
-        title:string;
-       
-
+    created_at: string;
+    id: string;
+    image_url: string;
+    is_premium: boolean;
+    is_published: boolean;
+    title: string;
+    tags: ITag[];
+    categories: ICategory[];
 } | null;
 
-export type Iuser ={
-        created_at: string;
-        display_name: string;
-        email: string;
-        id: string;
-        image_url: string;
-        role: string;
-        stripe_customer_id: string | null;
-        stripe_subscription_id: string | null;
-        subscription_status: boolean;
-    } | null;
+export type Iuser = {
+    created_at: string;
+    display_name: string;
+    email: string;
+    id: string;
+    image_url: string;
+    role: string;
+    stripe_customer_id: string | null;
+    stripe_subscription_id: string | null;
+    subscription_status: boolean;
+} | null;
